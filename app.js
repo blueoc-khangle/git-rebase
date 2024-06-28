75406b8d9f225475ac512326cc0ef9a937b87ff8
DESKTOP-D314UU8\khangle2307 <khang.le@blueoc.tech>
2024-06-28 11:16:08 +0700

    feat-6 was rebase

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function helloworld() {
   return "Hello World!";
 }
 
-const number1 = "1"
+const number1 = "11"
 
 const number2 = "2";
 
@@ -11,3 +11,15 @@ const number3 = "3";
 const number4 = "4"
 
 const number5 = "5";
+
+const number6 = "6";
+
+const number7 = "7";
+
+const number8 = "8";
+
+const number9 = "9"
+
+const number10 = "10"
+
+
